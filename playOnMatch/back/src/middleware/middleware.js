3cf5273d22dad9f13c6f56c8c2dd6b2b193a31e2
Joao-Augusto0 <j.august0rc0@gmail.com>
2023-04-11 08:54:19 -0300

    primeiro middleware pronto

---
+++ playOnMatch/back/src/middleware/middleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+const autorizacao = (req, res, next) => {
+    const token = req.headers.authorization
+    jwt.verify(token, process.env.KEY, (err, data) => {
+        if (err != null) {
+            res.status(404).json(err).end()
+        }else{
+            if(data.id === Number(req.params.id)){
+                next()
+            }else{
+                res.status(401).end()
+            }
+        }
+    })
+}
+
+module.exports = {
+    autorizacao
+}
