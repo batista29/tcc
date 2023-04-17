b0112c73f8af8f7c8e909e3338746fba829054bf
batista29 <natabatista2908@gmail.com>
2023-04-17 10:38:56 -0300

    Front encontros atualizado

--- playOnMatch/front/principal/script.js
+++ playOnMatch/front/principal/script.js
@@ -3,7 +3,9 @@ const readInfo = document.querySelector('.readInfo');
 const readLocal = document.querySelector('.readLocal');
 
 function carregar() {
-    const options = { method: 'GET' };
+    const options = {
+        method: 'GET'
+    };
 
     fetch('http://localhost:3000/listarEncontros', options)
         .then(response => response.json())
@@ -13,7 +15,14 @@ function carregar() {
                 let tabela = readInfo.cloneNode(true)
                 tabela.classList.remove("model")
                 tabela.querySelector('.descricao').innerHTML = 'descricao: ' + dados.descricao
-                tabela.querySelector('.data').innerHTML = 'data: ' + dados.data
+
+                var date = new Date(dados.data);
+                let dataFormatada = date.toLocaleDateString("pt-BR", {
+                    timeZone: "UTC",
+                });
+
+
+                tabela.querySelector('.data').innerHTML = 'data: ' + dataFormatada
                 tabela.querySelector('.titulo').innerHTML = 'titulo: ' + dados.titulo
                 tabela.querySelector('.nomeLocal').innerHTML = 'nomeLocal: ' + dados.local.nome
                 tabela.querySelector('.capacidadeLocal').innerHTML = 'capacidadeLocal: ' + dados.local.capacidade
@@ -23,4 +32,47 @@ function carregar() {
 
         })
         .catch(err => console.error(err));
+}
+
+function adicionarEncontro() {
+
+    let descricaoSubmit = document.querySelector("#descricaoSubmit").value
+    let dataSubmit = document.querySelector("#dataSubmit").value
+    let tituloSubmit = document.querySelector("#tituloSubmit").value
+    let id_localSubmit = document.querySelector("#id_localSubmit").value
+
+    let data = dataSubmit + "T00:00:00.000Z"
+
+    let dados = {
+        descricao: descricaoSubmit,
+        data: data,
+        titulo: tituloSubmit,
+        id_local: Number(id_localSubmit)
+    }
+
+    console.log(dados)
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dados)
+    };
+
+    fetch('http://localhost:3000/criarEncontro', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+}
+
+
+function abrirModal() {
+    let modalAparecer = document.querySelector(".readInferior");
+    modalAparecer.classList.add("modal")
+}
+
+function fecharModal() {
+    let modalAparecer = document.querySelector(".readInferior");
+    modalAparecer.classList.remove("modal")
 }
